54fe03533abb20b8a6cc8c9f92154aa028451779
Mitch Mommers <mitchoost@gmail.com>
2018-12-05 10:54:51 +0100

    Fixed pin number in comments

--- fs/init.js
+++ fs/init.js
@@ -8,11 +8,11 @@ load('api_sys.js');
 load('api_pwm.js');
 
 /*
-* We used pin 17 as an additional 3.3v power pin because our breadboards where a bit to small :D
+* We used pin 15 as an additional 3.3v power pin because our breadboards where a bit to small :D
 * But as you can see we got a workaround for that.
 */
 
-// ----------------------- THIS PART IS FOR 3.3v POWER ON PIN 17
+// ----------------------- THIS PART IS FOR 3.3v POWER ON PIN 15
 let ADDITIONAL_POWER = 15;
 
 GPIO.set_mode(ADDITIONAL_POWER, GPIO.MODE_OUTPUT);
